a4f2aac26395011ca85c94320565f39da80388d0
Manuel Cabassa <alexmannuel23@gmail.com>
2024-03-29 22:31:38 -0400

    Update HomeC

--- Portafolio/Controllers/HomeController.cs
+++ Portafolio/Controllers/HomeController.cs
@@ -2,6 +2,9 @@ using Microsoft.AspNetCore.Mvc;
 using Portafolio.Models;
 using Portafolio.Servicios;
 using System.Diagnostics;
+using System.Net.Mail;
+using System.Net;
+using System.Text;
 
 namespace Portafolio.Controllers
 {
@@ -9,37 +12,53 @@ namespace Portafolio.Controllers
     {
         private readonly ILogger<HomeController> _logger;
         private readonly IRepositorioProyectos repositorioProyectos;
+        private readonly IConfiguration _configuration;
 
-        public HomeController(ILogger<HomeController> logger, IRepositorioProyectos repositorioProyectos)
+        public HomeController(ILogger<HomeController> logger, IRepositorioProyectos repositorioProyectos, IConfiguration configuration)
         {
             _logger = logger;
             this.repositorioProyectos = repositorioProyectos;
+            this._configuration = configuration;
         }
 
         public IActionResult Index()
         {
-            _logger.LogInformation("Este es un mensaje de log");
-            var proyectos = repositorioProyectos.ObtenerProyectos().Take(4).ToList();
+            var proyectos = repositorioProyectos.ObtenerProyectos().Take(3).ToList();
             var model = new HomeIndexViewModel()
             {
                 Proyecto = proyectos,
-                
-            };
 
+            };
             return View(model);
         }
 
+        public IActionResult Proyectos()
+        {
+            var proyectos = repositorioProyectos.ObtenerProyectos();
+            return View(proyectos);
+        }
 
+        public IActionResult Contacto()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Contacto(ContacoViewModel contacoViewModel)
+        {
 
-        public IActionResult Privacy()
+            if (ModelState.IsValid)
+            {
+
+            }
+
+            return RedirectToAction("Gracias");
+        }
+
+        public IActionResult Gracias()
         {
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+
     }
 }
---
+++ Portafolio/Models/ContacoViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Portafolio.Models
+{
+    public class ContacoViewModel
+    {
+        public string Nombre { get; set; }
+        public string Email { get; set; }
+        public string Mensaje { get; set; }
+    }
+}
